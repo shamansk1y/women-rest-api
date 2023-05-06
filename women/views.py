88d5558e270c6b5fe8b31bf82893ced0136cba1a
shamansk1y <shamansky1225@gmail.com>
2023-05-06 19:43:40 +0200

    upd serializers, views

--- women/views.py
+++ women/views.py
@@ -8,21 +8,24 @@ from .models import Women
 from .serializers import WomenSerializer
 
 
-# class WomenAPIView(APIView):
-#     def get(self, request):
-#         lst = Women.objects.all().values()
-#         return Response({'posts': list(lst)})
-#
-#     def post(self, request):
-#         post_new = Women.objects.create(
-#             title=request.data['title'],
-#             content=request.data['content'],
-#             cat_id=request.data['cat_id']
-#         )
-#
-#         return Response({'post': model_to_dict(post_new)})
+class WomenAPIView(APIView):
+    def get(self, request):
+        w = Women.objects.all()
+        return Response({'posts': WomenSerializer(w, many=True).data})
+
+    def post(self, request):
+        serializer = WomenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        post_new = Women.objects.create(
+            title=request.data['title'],
+            content=request.data['content'],
+            cat_id=request.data['cat_id']
+        )
+
+        return Response({'post': WomenSerializer(post_new).data})
 
 
-class WomenAPIView(generics.ListAPIView):
-    queryset = Women.objects.all()
-    serializer_class = WomenSerializer
+# class WomenAPIView(generics.ListAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
